bfeebd6df7b63b9e76ef0be788b6b50192506b9d
Kai Hoppert <kai.hoppert@tomcom.de>
2013-10-22 14:42:48 +0200

    add fonts

--- tomcom/plone/base/__init__.py
+++ tomcom/plone/base/__init__.py
@@ -4,5 +4,12 @@ import logging
 logger = logging.getLogger('tomcom.plone.base')
 logger.debug('Installing Product')
 
+from Products.CMFCore.DirectoryView import registerFileExtension
+from Products.CMFCore.FSFile import FSFile
+
+registerFileExtension('woff', FSFile)
+registerFileExtension('eot', FSFile)
+registerFileExtension('ttf', FSFile)
+
 def initialize(context):
     """initialize product (called by zope)"""
