c6c89e88965215d05a2cb060d9590652c0295465
Kai Hoppert <kai.hoppert@tomcom.de>
2013-10-29 14:33:12 +0100

    Prepare tomcom.adapters.renderzpt 4.3.0.1.

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(name='tomcom.adapters.renderzpt',
                        open('CHANGES.rst').read(),
       classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.3',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
