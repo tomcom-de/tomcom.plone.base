ee791df3047f5504ddac969a33910d4db5f9be7e
Sebastian Bornschein <sebastian.bornschein@tomcom.de>
2013-11-04 10:27:27 +0100

    .

--- tomcom/plone/base/resource/js/toolbar.js
+++ tomcom/plone/base/resource/js/toolbar.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
         $('<div id="macro-globalactions-bar">&nbsp;</div>').insertAfter('.plone-toolbar #content-views');
 
-        $.post('tc_toolbar', function(data) {
+        $.post($('base').attr('href')+'/tc_toolbar', function(data) {
             $('#macro-globalactions-bar').html(data);
         });
 
