ff6be4ccc3c694590e308c5cf8ad2f2688d2e2f7
Kai Hoppert <kai.hoppert@tomcom.de>
2014-01-20 08:57:27 +0100

    .

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '4.3.1.1'
+version = '4.3.1.2'
 
 tests_require = [
     'plone.app.testing',
@@ -29,8 +29,8 @@ setup(name='tomcom.plone.base',
         'setuptools',
         'jarn.jsi18n',
         'tomcom.plone.ptregistry',
-        'tomcom.plone.fontawesome==4.0.3',
-        'tomcom.plone.bootstrap==3.0.2',
+        'tomcom.plone.fontawesome>=4.0.3',
+        'tomcom.plone.bootstrap>=3.0.2',
       ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require,
--- tomcom/plone/base/resource/js/helper.js
+++ tomcom/plone/base/resource/js/helper.js
@@ -3,10 +3,11 @@ $(document).ready(function () {
     $('body').delegate( '.no-click', 'click', function(event) {
         event.preventDefault();
     });
-    $('.custom-overlay-ajax').prepOverlay({
-        subtype: 'ajax'
-    });
-
+    if ($('.custom-overlay-ajax').length) {
+        $('.custom-overlay-ajax').prepOverlay({
+            subtype: 'ajax'
+        });
+    }
     if ($('.custom-select2').length) {
         if ($('.custom-select2')['select2'] != undefined) {
             $('.custom-select2').select2();
