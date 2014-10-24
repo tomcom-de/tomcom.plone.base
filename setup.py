6a068968a1ba5e69a3886029b658c433a3feecca
Kai Hoppert <kai.hoppert@tomcom.de>
2014-10-24 16:22:35 +0200

    .

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '4.3.1.12'
+version = '4.3.1.13'
 
 tests_require = [
     'plone.app.testing',
--- tomcom/plone/base/resource/js/toolbar.js
+++ tomcom/plone/base/resource/js/toolbar.js
@@ -1,6 +1,5 @@
 $.fn.tcpb_update_toolbar = function() {
     if(('.plone-toolbar').length){
-
         $('<div id="macro-globalactions-bar">&nbsp;</div>').insertAfter('.plone-toolbar #content-views');
         $.post($('base').attr('href')+'/tc_toolbar', function(data) {
             $('#macro-globalactions-bar').html(data);
