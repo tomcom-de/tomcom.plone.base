fe75471247e7e863344c04ec5b2dc2ae926cbc3e
Kai Hoppert <kai.hoppert@tomcom.de>
2014-12-22 10:38:14 +0100

    .

--- tomcom/plone/base/patches/image.py
+++ tomcom/plone/base/patches/image.py
@@ -1,5 +1,5 @@
 from Products.ATContentTypes.content.image import ATImage
 from plone.app.blob.subtypes.image import SchemaExtender
 
-ATImage.assocFileExt = ('jpg', 'jpeg', 'png', 'gif', 'tif', 'tiff', 'eps', 'wmf')
-SchemaExtender.fields[0].allowable_content_types=('image/gif', 'image/jpeg', 'image/png', 'image/tiff', 'application/postscript', 'image/x-wmf')
+ATImage.assocFileExt = ('jpg', 'jpeg', 'png', 'gif', 'tif', 'tiff', 'eps', 'wmf', 'bmp')
+SchemaExtender.fields[0].allowable_content_types=('image/gif', 'image/jpeg', 'image/png', 'image/tiff', 'application/postscript', 'image/x-wmf', 'image/x-ms-bmp')
